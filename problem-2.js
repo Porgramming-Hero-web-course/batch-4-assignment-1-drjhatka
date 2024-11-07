9d81ba51e672f4e2ab89bd51790da9355a1190ac
drjhatka <bishawjit12@gmail.com>
2024-11-07 19:50:05 -0400

    extra js file deleted

--- problem-2.js
+++
@@ -1,6 +0,0 @@
-//Create a TypeScript function removeDuplicates that accepts an array of numbers and 
-//returns a new array with duplicates removed. Preserve the original order of elements.
-var removeDuplicates = function (numberArray) {
-    return numberArray.filter(function (item, index, inputArray) { return inputArray.indexOf(item) == index; });
-};
-removeDuplicates([1, 2, 2, 3, 4, 4, 5]);
