de637cee56109acf5731b40a0ded7ec7dcc253d4
drjhatka <bishawjit12@gmail.com>
2024-11-07 19:49:05 -0400

    problem 4 needs to be refactored

--- problem-8.ts
+++ problem-8.ts
@@ -1,12 +1,13 @@
+{
 /*
 
 Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. 
 The function should return true if all of the specified keys exist in the object; otherwise, 
 it should return false.
 */
-{
-const validateKeys =<T extends {name:string, age:number,email:string}> (obj:T , keys:(keyof T)[])=>{
-    return (keys.toString()==Object.keys(obj).toString() )
+const validateKeys =<T extends {name:string, age:number,email:string}> (obj:T , keys:(keyof T)[]):boolean=>{
+    //const value = (Object.keys(obj).every(k=>keys.includes(k)) )
+    return keys.every(key=>key in obj)
 }
 
 const person = { name: "Alice", age: 25, email: "alice@example.com" };
